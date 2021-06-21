23d9bef03c82d22e8f6b976dac2c1602b0b8f6a5
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-21 20:04:10 +0530

    integrated google maps

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from './User';
+import { Company } from './Company';
+
+const mapDiv = document.getElementById('map');
+
+const map = new google.maps.Map(mapDiv, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
